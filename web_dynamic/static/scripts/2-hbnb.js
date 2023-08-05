d709f585b727997c450657dab5ef80586f1ea609
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-08-05 16:27:10 +0100

    (feat) -> feat all places from the front-end using ajax

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,36 @@
-$(window).on('load', () => {
+$(document).ready(() => {
+  const amenity_ids = [];
+  const amenity_names = [];
+
+  $('.popover ul input').on('change', (e) => {
+    setChecked(e.target.checked, e.target.dataset, amenity_ids, amenity_names);
+    $('.amenities h4').text(amenity_names.join(', '));
+  });
+
+  const setChecked = (checked, value = {}, idArray = [], namesArray = []) => {
+    if (checked) {
+      addItem(idArray, namesArray, value);
+    } else {
+      removeItem(idArray, namesArray, value);
+    }
+  };
+
+  const removeItem = (idArray = [], namesArray = [], value = {}) => {
+    const index = idArray.indexOf(value.id);
+    if (index !== -1) {
+      idArray.splice(index, 1);
+      namesArray.splice(index, 1);
+    } else {
+      throw new Error('Id not in list');
+    }
+  };
+
+  const addItem = (idArray = [], namesArray = [], value = {}) => {
+    idArray.push(value.id);
+    namesArray.push(value.name);
+  };
+
+  /* get api status */
   try {
     $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
       if (data) {
